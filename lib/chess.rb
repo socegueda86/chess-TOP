6ea4ed01f3a6ef791b223e8de03d011793e888a2
socegueda86 <socegueda86@gmail.com>
2022-11-03 22:34:04 -0600

    Create @white_pieces, @current_player, switch_player method and new_index method.
    @white_pieces help us determine who have each color.
    
    @current_player will help us during the program to know who is the
    current player.
    
    switch_player method help us alternate between methods. It uses
    new index as a helper method to change the index.

--- lib/chess.rb
+++ lib/chess.rb
@@ -2,6 +2,9 @@ class Game
   def initialize(player_1_class, player_2_class)
     @board = create_board()
     @players = [player_1_class.new, player_2_class.new]
+    @white_pieces = @players[rand(0..1)]
+    @current_player = @white_pieces
+
   end
 
   def create_board
@@ -16,6 +19,16 @@ class Game
       draw_board_whites_perspective
     end
   end
+
+  def switch_player
+    @current_player = @players[new_index]
+  end
+
+  def new_index
+    return 1 if @players.find_index(@current_player) == 0
+    return 0 if @players.find_index(@current_player) == 1
+  end
+
 end
 
 
@@ -23,3 +36,9 @@ class Player;end
 
 class HumanPlayer;end
 class ComputerPlayer;end
+
+class Pieces
+  def initialize(game)
+    @game = game
+  end
+end
