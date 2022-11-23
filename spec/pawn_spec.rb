066d9b36dae8e44c3c23eb93f0cdbf2af48cbbaf
socegueda86 <socegueda86@gmail.com>
2022-11-23 11:56:33 -0600

    Erase pieces_spec.rb

---
+++ spec/pawn_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../lib/pawn.rb'
+require_relative '../lib/chess.rb'
+require_relative '../lib/moves_module.rb'
+require_relative '../lib/draw_board_module.rb'
+
+RSpec.describe Pawn do
+
+  include MovesModule
+  include DrawBoard
+
+  
+
+
+
+end
